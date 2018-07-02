b1154beb3473fa59c49d3ee277f611e762809f26
giginet <giginet.net@gmail.com>
2018-07-03 00:31:23 +0900

    Add Homebrew formula

---
+++ Formula/libjwt.rb
@@ -0,0 +1,36 @@
+class Libjwt < Formula
+  desc "JSON Web Token C library"
+  homepage "https://github.com/benmcollins/libjwt"
+  url "https://github.com/giginet/libjwt/archive/v2.0.0.tar.gz"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "jansson"
+  depends_on "openssl"
+
+  def install
+    system "autoreconf", "-fiv"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "all"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdlib.h>
+      #include <jwt.h>
+
+      int main() {
+        jwt_t *jwt = NULL;
+        if (jwt_new(&jwt) != 0) return 1;
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-I#{include}", "-ljwt", "-o", "test"
+    system "./test"
+  end
+end
